9ab631136370fa2b6918222816b8464dc1f53ba2
YUTAKURE <shiro.white.ffffff@gmail.com>
2024-07-18 14:31:59 -0700

    ログイン機能

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import type { Session } from '@supabase/auth-helpers-nextjs';
 
 // ナビゲーション
-const Navigation = ({ session }: { session: Session | null }) => {
+const Navigation = ({ session }: { session?: Session | null }) => {
   return (
     <header className="shadow-lg shadow-gray-100">
       <div className="py-5 container max-w-screen-sm mx-auto flex items-center justify-between">
@@ -22,7 +22,7 @@ const Navigation = ({ session }: { session: Session | null }) => {
           ) : (
             <div className="flex items-center space-x-5">
               <Link href="/auth/login">ログイン</Link>
-              <Link href="/auth/signup">サインアップ</Link>
+              <Link href="/auth/signup">会員登録</Link>
             </div>
           )}
         </div>
